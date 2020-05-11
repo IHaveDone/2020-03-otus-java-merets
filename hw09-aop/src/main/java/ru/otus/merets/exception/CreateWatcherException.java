36c177a5bded9c4502af6d6242bfe6cdcca3e438
Artem Merets <artem@merets.ru>
2020-05-11 18:56:57 +0300

    hw09, AOP, remove exception (I need no longer any exceptions in this code)

--- hw09-aop/src/main/java/ru/otus/merets/exception/CreateWatcherException.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.merets.exception;
-
-public class CreateWatcherException extends RuntimeException {
-    public CreateWatcherException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- hw09-aop/src/main/java/ru/otus/merets/watcher/Watcher.java
+++ hw09-aop/src/main/java/ru/otus/merets/watcher/Watcher.java
@@ -1,13 +1,10 @@
 package ru.otus.merets.watcher;
 
-import ru.otus.merets.exception.CreateWatcherException;
-
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
-
 public class Watcher<T> {
-    public <T> T createProxedObject(T childClass) throws CreateWatcherException {
+    public <T> T createProxedObject(T childClass) {
         InvocationHandler handler = new InvocationHandlerImpl<T>(childClass );
         return (T) Proxy.newProxyInstance(Watcher.class.getClassLoader(),
                 childClass.getClass().getInterfaces(), handler);
