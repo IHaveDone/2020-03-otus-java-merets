18365bf65c0f3cd8b2bcb8a7d59a7d520d327e5d
Artem Merets <artem@merets.ru>
2020-06-09 23:40:03 +0300

    hw17, jdbc mapper, make it a little simpler

--- hw15-serialization/src/main/java/ru/otus/merets/mygson/MyGson.java
+++ hw15-serialization/src/main/java/ru/otus/merets/mygson/MyGson.java
@@ -3,6 +3,7 @@ package ru.otus.merets.mygson;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Here and later we think that {@code MyGson} is a part of Json
@@ -12,9 +13,13 @@ import java.util.*;
  */
 public class MyGson {
     /**
-     * Each MyGson can has some key->value pairs
+     * Each MyGson can have some key->value pairs
      */
     private Map<String, Object> map;
+
+    private final Class<?>[] defaults = {Integer.class, Character.class, Float.class, Double.class, Boolean.class, Byte.class, String.class,
+            int.class, char.class, float.class, double.class, boolean.class, byte.class};
+    private final Class<?>[] quotesClasses = {String.class, Character.class};
 
     /**
      * Determine is the {@code clazz} a basic class or not
@@ -23,8 +28,6 @@ public class MyGson {
      * @return true if {@code clazz} is a basic Class (String, Integer, etc.)
      */
     private boolean isDefaultType(Class<?> clazz) {
-        Class<?>[] defaults = {Integer.class, Character.class, Float.class, Double.class, Boolean.class, Byte.class, String.class,
-                                int.class, char.class, float.class, double.class, boolean.class, byte.class};
         return Arrays.asList(defaults).contains(clazz);
     }
 
@@ -32,15 +35,16 @@ public class MyGson {
      * Actually I do not need default constructor, the task says
      * we should use it like MyGson myGson = new MyGson();
      * so I have to keep it.
+     * I was wrong, default constructor is necessary for this case :-]
      */
-    public MyGson(){
+    public MyGson() {
     }
 
     /**
      * Simple wrapper around the class
+     *
      * @param obj {@code Object} that should be converted to Json
      * @return String in Json format
-     * @throws IllegalAccessException
      */
     public String toJson(Object obj) {
         init(obj);
@@ -51,8 +55,8 @@ public class MyGson {
      * Private constructor to make easier usage during parsing,
      * no {@code MyGson myGson = new MyGson(); myGson.toJson(object);}
      * just {@code new MyGson(Object);}
+     *
      * @param object {@code Object} that should be translated to Json
-     * @throws IllegalAccessException
      */
     private MyGson(Object object) {
         init(object);
@@ -60,61 +64,53 @@ public class MyGson {
 
     /**
      * Parse Object[] value and put it into the map
-     * @param keyOfField
-     * @param valueOfField
+     *
+     * @param keyOfField - key
+     * @param valueOfField - value
      */
-    private void fillMapThroughObjectArray(String keyOfField, Object[] valueOfField){
-        List<Object> objs = new ArrayList<>();
-        if( Array.getLength(valueOfField)>0) {
-            for (Object currentObject :valueOfField) {
-                if(isDefaultType( currentObject.getClass() )){
-                    objs.add(currentObject);
-                } else {
-                    objs.add(new MyGson(currentObject));
-                }
-            }
-            map.put(keyOfField, objs);
-        } else {
-            map.put(keyOfField,null);
+    private void fillMapThroughObjectArray(String keyOfField, Object[] valueOfField) {
+        if (Array.getLength(valueOfField) == 0) {
+            map.put(keyOfField, null);
+            return;
         }
+        fillMapThroughCollection(keyOfField, Arrays.asList(valueOfField));
     }
+
 
     /**
      * Parse Array[] and put values into the map
+     *
      * @param keyOfField
      * @param valueOfField
      */
-    private void fillMapThroughDefaultArray(String keyOfField, Object[] valueOfField){
-        final List<Object> innerArray = new ArrayList<>();
-        for (int index = 0; index < Array.getLength(valueOfField); index++) {
-            innerArray.add(Array.get(valueOfField, index));
-        }
-        map.put(keyOfField, innerArray);
+    private void fillMapThroughDefaultArray(String keyOfField, Object[] valueOfField) {
+        map.put(keyOfField, Arrays.asList(valueOfField));
     }
 
     /**
      * Iterate Collection and put values into the map
+     *
      * @param keyOfField
-     * @param col
+     * @param col - Collection of objects
      */
-    private void fillMapThroughCollection(String keyOfField, Collection<Object> col){
-        if (col.size() > 0) {
-            final List<Object> innerArray = new ArrayList<>();
-            for (Object o : col) {
-                if (isDefaultType(o.getClass())) {
-                    innerArray.add(o);
-                } else {
-                    innerArray.add(new MyGson(o));
-                }
-            }
-            map.put(keyOfField, innerArray);
-        } else {
+    private void fillMapThroughCollection(String keyOfField, Collection<Object> col) {
+        if (col.size() == 0) {
             map.put(keyOfField, null);
         }
+        final List<Object> innerArray = new ArrayList<>();
+        col.forEach((p) -> {
+            if (isDefaultType(p.getClass())) {
+                innerArray.add(p);
+            } else {
+                innerArray.add(new MyGson(p));
+            }
+        });
+        map.put(keyOfField, innerArray);
     }
 
     /**
      * Takes {@code Object} and tries to build Json from it.
+     *
      * @param object
      * @throws IllegalAccessException
      */
@@ -139,16 +135,16 @@ public class MyGson {
             if (valueOfField == null) {
                 map.put(keyOfField, null);
             } else if (typeOfField.isArray()) {
-                if (valueOfField instanceof Object[] ) {
-                    fillMapThroughObjectArray( keyOfField, (Object[]) valueOfField );
+                if (valueOfField instanceof Object[]) {
+                    fillMapThroughObjectArray(keyOfField, (Object[]) valueOfField);
                 } else {
-                    fillMapThroughDefaultArray( keyOfField, (Object[]) valueOfField );
+                    fillMapThroughDefaultArray(keyOfField, (Object[]) valueOfField);
                 }
             } else {
-                if ( isDefaultType(typeOfField )) {
+                if (isDefaultType(typeOfField)) {
                     map.put(keyOfField, valueOfField);
                 } else if (Arrays.asList(typeOfField.getInterfaces()).contains(Collection.class)) {
-                    fillMapThroughCollection( keyOfField, (Collection<Object>) valueOfField);
+                    fillMapThroughCollection(keyOfField, (Collection<Object>) valueOfField);
                 } else {
                     map.put(keyOfField, new MyGson(valueOfField));
                 }
@@ -158,17 +154,16 @@ public class MyGson {
     }
 
     /**
-     *
      * @param clazz
      * @return true if {@code clazz} should be shielded via quotes
      */
     private boolean needQuotes(Class<?> clazz) {
-        Class<?>[] quotesClasses = {String.class, Character.class};
         return Arrays.asList(quotesClasses).contains(clazz);
     }
 
     /**
      * Convert the value to string format
+     *
      * @param object
      * @return String for Json format
      */
@@ -178,7 +173,7 @@ public class MyGson {
         }
         StringBuilder sb = new StringBuilder();
         if (needQuotes(object.getClass())) {
-            sb.append("\"" + object.toString() + "\"");
+            sb.append("\"").append(object.toString()).append("\"");
         } else {
             sb.append(object.toString());
         }
@@ -187,32 +182,30 @@ public class MyGson {
 
     /**
      * Convert {@code list} to Json format
+     *
      * @param list
      * @return String for Json format (with delimiters and brackets)
      */
     private String listToString(List<Object> list) {
-        StringBuilder sb = new StringBuilder();
-        for (Object o : list) {
-            if (sb.length() > 0) sb.append(",");
-            sb.append(elementToString(o));
-        }
-        return "[" + sb.toString() + "]";
+        return "[" + list.stream().map((p) -> elementToString(p)).collect(Collectors.joining(",")) + "]";
     }
 
     /**
      * Convert {@code MyGson} to String (Json format)
+     *
      * @return Json-string
      */
     public String toString() {
         if (map == null || map.size() == 0) {
             return "{}";
         }
+
         StringBuilder json = new StringBuilder();
         for (Map.Entry<String, Object> entry : map.entrySet()) {
             if (json.length() > 0) {
                 json.append(",");
             }
-            json.append("\"" + entry.getKey() + "\":");
+            json.append("\"").append(entry.getKey()).append("\":");
             if (entry.getValue() == null) {
                 json.append("null");
             } else if (Arrays.asList(entry.getValue().getClass().getInterfaces()).contains(List.class)) {
