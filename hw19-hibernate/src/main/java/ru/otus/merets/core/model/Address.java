e7a3ee2dc4111432dadd1ca7867f8c9d96e0061b
Artem Merets <artem@merets.ru>
2020-06-28 23:07:38 +0300

    hibernate markup & using

---
+++ hw19-hibernate/src/main/java/ru/otus/merets/core/model/Address.java
@@ -0,0 +1,38 @@
+package ru.otus.merets.core.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "addresses")
+public class Address {
+    @Id
+    @GenericGenerator(name = "address_generator", strategy = "increment")
+    @GeneratedValue(generator = "address_generator")
+    @Column(name="address_id")
+    private long id;
+
+    @Column(name = "street")
+    private String street;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="user_id")
+    private User user;
+
+    public Address(String street) {
+        this.id = 0;
+        this.street = street;
+    }
+
+    public Address() {
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", street='" + street + '\'' +
+                '}';
+    }
+}
