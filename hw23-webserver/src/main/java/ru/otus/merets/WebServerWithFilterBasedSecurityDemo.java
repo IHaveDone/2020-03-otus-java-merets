c6723d1b51ba792f9571851e15d12bfa568625ea
Artem Merets <artem@merets.ru>
2020-07-26 23:18:17 +0300

    return port 8080

--- hw23-webserver/src/main/java/ru/otus/merets/WebServerWithFilterBasedSecurityDemo.java
+++ hw23-webserver/src/main/java/ru/otus/merets/WebServerWithFilterBasedSecurityDemo.java
@@ -19,7 +19,7 @@ import ru.otus.merets.service.UserAuthServiceImpl;
 import ru.otus.merets.server.UsersWebServerWithFilterBasedSecurity;
 
 public class WebServerWithFilterBasedSecurityDemo {
-    private static final int WEB_SERVER_PORT = 8089;
+    private static final int WEB_SERVER_PORT = 8080;
     private static final String TEMPLATES_DIR = "/templates/";
 
     public static void main(String[] args) throws Exception {
